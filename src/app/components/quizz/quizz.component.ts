f7ffcf13ef5642e7279385cc6e8747b139e13143
Davi Viana <89869115+davifv22@users.noreply.github.com>
2023-12-06 21:28:51 -0300

    project

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { quizz_questions } from "../../../assets/data/quizz_questions"
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions[0]){
+      this.finished = false
+      this.title = quizz_questions[0]['title']
+
+      this.questions = quizz_questions[0]['questions']
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quizz_questions[0]['results'][finalAnswer as keyof typeof quizz_questions[0]['results'] ]
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+        if(
+          arr.filter(item => item === previous).length >
+          arr.filter(item => item === current).length
+        ){
+          return previous
+        }else{
+          return current
+        }
+    })
+
+    return result
+  }
+
+}
